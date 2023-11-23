1d81179a8905b034c57b96d6dc219f502ae07e1c
Serhii <martinenko.sergiy@gmail.com>
2023-11-23 16:24:39 +0200

    added base styles and more crud functions

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import get_user_model
 from django.forms import ModelForm
+from django import forms
 
 from kitchen.models import Dish, DishType
 
@@ -8,8 +10,21 @@ class DishForm(ModelForm):
         model = Dish
         fields = "__all__"
 
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
 
 class DishTypeForm(ModelForm):
     class Meta:
         model = DishType
         fields = "__all__"
+
+
+class CookForm(ModelForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ("years_of_experience", "username", "first_name", "last_name")
--- kitchen/urls.py
+++ kitchen/urls.py
@@ -9,6 +9,9 @@ from kitchen.views import (
     DishUpdateView,
     CookListView,
     CookDetailView,
+    CookCreateView,
+    CookDeleteView,
+    CookUpdateView,
     DishTypeDetailView,
     DishTypeListView,
     DishTypeCreateView,
@@ -30,6 +33,9 @@ urlpatterns = [
     path("dishes-types/<int:pk>/update", DishTypeUpdateView.as_view(), name="dish-type-update"),
     path("cooks/", CookListView.as_view(), name="cook-list"),
     path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
+    path("cooks/create/", CookCreateView.as_view(), name="cook-create"),
+    path("cooks/<int:pk>/delete", CookDeleteView.as_view(), name="cook-delete"),
+    path("cooks/<int:pk>/update", CookUpdateView.as_view(), name="cook-update"),
 ]
 
 app_name = "kitchen"
